6c3aac4386fd59da7c423fa89ddecf288e062a0a
Ahmad Mujahid <ahmadm.abdurrahman@gmail.com>
2023-11-30 01:47:10 +0800

    (scheduler): setup csv importer

--- car-park-scheduler/src/test/java/com/ahmad/carparkscheduler/CarParkSchedulerApplicationTests.java
+++ car-park-scheduler/src/test/java/com/ahmad/carparkscheduler/CarParkSchedulerApplicationTests.java
@@ -1,12 +1,15 @@
 package com.ahmad.carparkscheduler;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import com.ahmad.carparkscheduler.persister.CarParkInfo;
-import com.ahmad.carparkscheduler.persister.CarParkService;
+import com.ahmad.carparkscheduler.csv.carparkinfo.CarParkInfoCSV;
+import com.ahmad.carparkscheduler.csv.CarParkCSVService;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,7 +22,7 @@ import reactor.test.StepVerifier;
 class CarParkSchedulerApplicationTests {
 
   @InjectMocks
-  private CarParkService carParkService;
+  private CarParkCSVService carParkCSVService;
 
   @Mock
   private ResourceLoader resourceLoader;
@@ -29,30 +32,45 @@ class CarParkSchedulerApplicationTests {
 
   @Test
   void readCarParkInfoFile_ShouldReturnCarParkInfos() throws IOException {
-    final String csvData = "ACB,BLK 270/271 ALBERT CENTRE BASEMENT CAR PARK,30314.7936,31490.4942,BASEMENT CAR PARK,ELECTRONIC PARKING,WHOLE DAY,NO,YES,1,1.80,Y";
-    final CarParkInfo expectedCarParkInfo = CarParkInfo.builder()
-        .carParkNo("ACB")
-        .address("BLK 270/271 ALBERT CENTRE BASEMENT CAR PARK")
-        .xCoord("30314.7936")
-        .yCoord("31490.4942")
-        .carParkType("BASEMENT CAR PARK")
-        .typeOfParkingSystem("ELECTRONIC PARKING")
-        .shortTermPark("WHOLE DAY")
-        .freeParking("NO")
-        .nightParking("YES")
-        .carParkDecks("1")
-        .gantryHeight("1.80")
-        .carParkBasement("Y")
-        .build();
+    final String csvData = "car_park_no,address,x_coord,y_coord,car_park_type,type_of_parking_system,short_term_parking,free_parking,night_parking,car_park_decks,gantry_height,car_park_basement\n"
+        + "ACB,BLK 270/271 ALBERT CENTRE BASEMENT CAR PARK,30314.7936,31490.4942,BASEMENT CAR PARK,ELECTRONIC PARKING,WHOLE DAY,NO,YES,1,1.80,Y";
+    final List<CarParkInfoCSV> expectedCarParkInfoCSV = List.of(
+        CarParkInfoCSV.builder()
+            .carParkNo("ACB")
+            .address("BLK 270/271 ALBERT CENTRE BASEMENT CAR PARK")
+            .xCoord(30314.7936)
+            .yCoord(31490.4942)
+            .carParkType("BASEMENT CAR PARK")
+            .typeOfParkingSystem("ELECTRONIC PARKING")
+            .shortTermPark("WHOLE DAY")
+            .freeParking("NO")
+            .nightParking("YES")
+            .carParkDecks("1")
+            .gantryHeight("1.80")
+            .carParkBasement("Y")
+            .build()
+    );
 
     final InputStream inputStream = new ByteArrayInputStream(csvData.getBytes());
     when(resourceLoader.getResource("classpath:/static/HDBCarparkInformation.csv")).thenReturn(
         resource);
     when(resource.getInputStream()).thenReturn(inputStream);
 
-    StepVerifier.create(carParkService.readCarParkInfoFile())
-        .expectNextMatches(
-            carParkInfo -> carParkInfo.equals(expectedCarParkInfo))
-        .verifyComplete();
+    assertThat(carParkCSVService.readCarParkInfoFile()).isEqualTo(expectedCarParkInfoCSV);
+  }
+
+  @Test
+  void getFileLength() throws IOException {
+    final String csvData = "car_park_no,address,x_coord,y_coord,car_park_type,type_of_parking_system,short_term_parking,free_parking,night_parking,car_park_decks,gantry_height,car_park_basement\n"
+        + "ACB,BLK 270/271 ALBERT CENTRE BASEMENT CAR PARK,30314.7936,31490.4942,BASEMENT CAR PARK,ELECTRONIC PARKING,WHOLE DAY,NO,YES,1,1.80,Y\n"
+        + "ACM,BLK 98A ALJUNIED CRESCENT,33758.4143,33695.5198,MULTI-STOREY CAR PARK,ELECTRONIC PARKING,WHOLE DAY,SUN & PH FR 7AM-10.30PM,YES,5,2.10,N\n"
+        + "AH1,BLK 101 JALAN DUSUN,29257.7203,34500.3599,SURFACE CAR PARK,ELECTRONIC PARKING,WHOLE DAY,SUN & PH FR 7AM-10.30PM,YES,0,0.00,N\n"
+        + "AK19,BLOCK 253 ANG MO KIO STREET 21,28185.4359,39012.6664,SURFACE CAR PARK,COUPON PARKING,7AM-7PM,NO,NO,0,0.00,N";
+    final InputStream inputStream = new ByteArrayInputStream(csvData.getBytes());
+    when(resourceLoader.getResource("classpath:/static/HDBCarparkInformation.csv")).thenReturn(
+        resource);
+    when(resource.getInputStream()).thenReturn(inputStream);
+
+    assertThat(carParkCSVService.getFileLength()).isEqualTo(4);
   }
 }
